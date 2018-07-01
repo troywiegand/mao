08bd360a17d06ece24e399cd70e5bade1145f465
Troy <troy@troywiegand.com>
2018-07-01 16:34:17 -0400

    Introduce a constructor to have our rules display on a button click

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+class App{
+    constructor(){
+        this.OurRules=[]
+        this.ourRulesListFiller()
+        console.log(this.OurRules)
+        this.rulesButton= document.querySelector('#ourRules')
+        this.rulesButton.addEventListener('click', (eve)=>{
+            ev.preventDefault()
+            this.ourRulesListRender()
+
+        })
+    }
+
+    ourRulesListFiller(){
+        this.OurRules.push(this.newRule('Mario','Player must exclaim "MARIO!" whenever they play a `3`.'))
+
+    }
+
+    newRule(title,rulesText){
+
+        const rule= {
+            title: title,
+            rule: rulesText,
+        }
+        
+        return rule
+
+    }
+
+    ourRulesListRender(){
+
+    }
+
+   
+
+
+
+}
+
+
+const app = new App()
+
 /*
 ♠	
 ♥	
