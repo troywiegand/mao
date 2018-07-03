e34dd1dc09494ecda9f546028e922a7da0d98d3e
Troy <troy@troywiegand.com>
2018-07-03 08:47:55 -0400

    Allow the Remove Rule button to delete the rule from the page

--- src/ListItemMaker.js
+++ src/ListItemMaker.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
 class ListItemMaker extends Component{
+    constructor(){
+        super()
+        this.state={alive: true}
+    }
+
+    ruleKiller(){
+        if(this.state.alive){
+            this.setState({alive: false})
+        }
+    }
 
     render(){
+        if(this.state.alive){
         return ( 
         <div>
         <li>
             <h3>{this.props.title} </h3>
             <p>{this.props.rule}</p>
-            <button> Remove Rule</button>
+            <button onClick={(ev) =>{
+                ev.preventDefault()
+                this.ruleKiller()}}> 
+                Remove Rule</button>
 
         </li>
         </div>)}
+        return null
+    }
 
 
 
